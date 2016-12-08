5596cf64dc4f6d9f9dad9a3222e1668b8b4b65d2
Aman Bhimani <amanb014@gmail.com>
2016-12-08 11:25:21 -0500

    Blog post excerpts
    Added backgrounds to the excerpts, padding, box-shadow, and styled the
    title.
    
    Minor update to functions, and header.php (made media queries work from
    common)

--- content-excerpt.php
+++ content-excerpt.php
@@ -1,11 +1,10 @@
-<div id="wrapper80">
+
 	<div class="blog-post">
 		
 		<h2 class="blog-post-title"><?php the_title(); ?></h2>
-		
-		<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
-	 <?php the_excerpt(); ?>
+	 <?php the_excerpt(15); ?>
+
+	 <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
 	</div>
-</div>
--- home.php
+++ home.php
@@ -1,13 +1,15 @@
 <?php get_header(); ?>
 	
-<?php 
-	if ( have_posts() ) {
-		while ( have_posts() ) {
-			the_post(); 
-			get_template_part( 'content-excerpt', get_post_format() );
-		
-		} // end while
-	} // end if
-?>
+<div class="wrapper80 max800">
+	<?php 
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post(); 
+				get_template_part( 'content-excerpt', get_post_format() );
+			
+			} // end while
+		} // end if
+	?>
+</div>
 	
 <?php get_footer(); ?>
