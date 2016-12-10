190eceffe72adc7ab40f28eeba7efb901c3f0526
Aman Bhimani <amanb014@gmail.com>
2016-12-10 14:06:27 -0500

    Removed footer stuff.

--- footer.php
+++ footer.php
@@ -2,37 +2,12 @@
 	<footer>
 		<div class="wrapper90 footer_container">
 			<div class="footer_item">
-				<h1>Stuff</h1>
-
-				<?php 
-					wp_nav_menu( 
-						array( 
-							'theme_location' => 'footer-left',
-							'menu_class'     => 'footer-menu'
-							) 
-					); 
-				?>
-			</div>
-
-
-			<div class="footer_item">
-
-				<h1>GET SOCIAL!</h1>
-				
-
 			</div>
 
 			<div class="footer_item">
-				<h1>Another stuff</h1>
+			</div>
 
-				<?php 
-					wp_nav_menu( 
-						array( 
-							'theme_location' => 'footer-right',
-							'menu_class'     => 'footer-menu'
-							) 
-					); 
-				?>
+			<div class="footer_item">
 			</div>
 
 		</div>
--- single.php
+++ single.php
@@ -6,29 +6,29 @@
 
 		<?php
 		// Start the loop.
-		while ( have_posts() ) : the_post();
+			while ( have_posts() ) : the_post();
 
-			/*
-			 * Include the post format-specific template for the content. If you want to
-			 * use this in a child theme, then include a file called called content-___.php
-			 * (where ___ is the post format) and that will be used instead.
-			 */
-			get_template_part( 'content-full', get_post_format() );
+				/*
+				 * Include the post format-specific template for the content. If you want to
+				 * use this in a child theme, then include a file called called content-___.php
+				 * (where ___ is the post format) and that will be used instead.
+				 */
+				get_template_part( 'content-full', get_post_format() );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			// if ( comments_open() || get_comments_number() ) :
-			// 	comments_template();
-			// endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				// if ( comments_open() || get_comments_number() ) :
+				// 	comments_template();
+				// endif;
 
-			// Previous/next post navigation.
-			the_post_navigation( array(
-				'type' => 'list',
-				'next_text' => 'Next Post',
-				'prev_text' => 'Previous Post'
-			) );
+				// Previous/next post navigation.
+				the_post_navigation( array(
+					'type' => 'list',
+					'next_text' => 'Next Post',
+					'prev_text' => 'Previous Post'
+				) );
 
-		// End the loop.
-		endwhile;
+			// End the loop.
+			endwhile;
 		?>
 
 		</main><!-- .site-main -->
