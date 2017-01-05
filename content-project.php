ea71cc56485896aad179f84541a24f82d8b67c52
Aman Bhimani <amanb014@gmail.com>
2017-01-05 17:21:19 -0500

    Removed single-project and now project posts use the single.php. And the code gets the post type content to use.

---
+++ content-project.php
@@ -0,0 +1,8 @@
+<div class="blog-post">
+	<h1 class="blog-post-title"><?php the_title(); ?></h1>
+
+	<h2 class="blog-post-text"><?php the_content(); ?></h2>
+
+	<p class="blog-post-meta"></p>
+
+</div>
